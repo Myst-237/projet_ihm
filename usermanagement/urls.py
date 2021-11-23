fd7f1b2a182cf5923d11cf88ea38bd65516e2a06
Cadell Griffith <cadellgriffith@Cadells-MacBook-Air.local>
2021-11-23 02:15:18 +0100

    receptionist view

--- usermanagement/urls.py
+++ usermanagement/urls.py
@@ -1,9 +1,20 @@
 from django.urls import path, include
-from .views import index,home
+from .views import (index,home,receptionist,receptionist_all_patients,book_appointment,add_payment,modify_appointment,
+                    modify_payment,payments,patient,doctor,nurse)
 
 app_name = 'usermanagement'
 
 urlpatterns = [
     path('', index , name="index"),
     path('home/', home , name="home"),
+    path('receptionist/', receptionist , name="receptionist"),
+    path('doctor/', doctor , name="doctor"),
+    path('patient/', patient , name="patient"),
+    path('nurse/', nurse , name="nurse"),
+    path('receptionist/all_patients/', receptionist_all_patients , name="receptionist_all_patients"),
+    path('receptionist/book_appointment/', book_appointment , name="book_appointment"),
+    path('receptionist/modify_appointment/', modify_appointment, name="modify_appointment"),
+    path('receptionist/add_payment', add_payment, name="add_payment"),
+    path('receptionist/modify_payment', modify_payment, name="modify_payment"),
+    path('receptionist/payments', payments, name="payments")
 ]
--- usermanagement/views.py
+++ usermanagement/views.py
@@ -1,28 +1,60 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from django.contrib.auth.decorators import login_required
+from django.contrib import messages
+from .models import CustomUser, Receptionist, Patient, Doctor
 # Create your views here.
-Roles = [
-    ('Doctor', 'Doctor'),
-    ('Patient', 'Patient'),
-    ('Receptionist', 'Receptionist'),
-    ('Admin', 'Admin'),
-    ('Accountant', 'Accountant'),
-    ('Nurse', 'Nurse'),
-    ('Labtech', 'Labtech'),
-    ('HRM', 'HRM'),
-    ('Specialist', 'Specialist'),
-]
 
 def index(request):
     return render(request, 'usermanagement/index.html')
 
 @login_required
 def home(request):
-    role_list = []
-    for object1,object2 in Roles:
-        role_list.append(object1)
-    
+    roles = request.user.role
+    print(roles)
     context = {
-        'role_list' : role_list,
+        'roles': roles
     }
     return render(request, 'usermanagement/home.html',context)
+
+@login_required
+def receptionist(request):
+        return render(request, 'usermanagement/receptionist.html')
+    
+
+@login_required
+def receptionist_all_patients(request):
+    return render(request, 'usermanagement/receptionist-all-patients.html')
+
+@login_required
+def book_appointment(request):
+    return render(request, 'usermanagement/book-appointment.html')
+
+@login_required
+def modify_appointment(request):
+    return render(request, 'usermanagement/modify-appointment.html')
+
+@login_required
+def add_payment(request):
+    return render(request, 'usermanagement/add-payment.html')
+
+@login_required
+def modify_payment(request):
+    return render(request, 'usermanagement/modify-payment.html')
+
+@login_required
+def payments(request):
+    return render(request, 'usermanagement/payments.html')
+    
+@login_required
+def patient(request):
+        return render(request, 'usermanagement/patient.html')
+    
+@login_required
+def doctor(request):
+        return render(request, 'usermanagement/doctor.html')
+    
+@login_required
+def nurse(request):
+        return render(request, 'usermanagement/nurse.html')
+    
+    
